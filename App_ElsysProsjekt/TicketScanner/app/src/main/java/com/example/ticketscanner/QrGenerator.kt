45b5b0b8f345aee88baceb109a0fdf37b80054fe
stian <stiahh@stud.ntnu.no>
2026-02-19 15:24:45 +0100

    2 implementations, 3 permissions og QrGenerator

--- App_ElsysProsjekt/TicketScanner/app/src/main/java/com/example/ticketscanner/QrGenerator.kt
+++ App_ElsysProsjekt/TicketScanner/app/src/main/java/com/example/ticketscanner/QrGenerator.kt
@@ -1,1 +1,28 @@
+package com.example.ticketscanner
 
+import android.graphics.Bitmap
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
+
+class QrGenerator {
+
+
+    fun getBitmapFromString(text: String): Bitmap?{
+        return try {
+            val multiFormatWriter = MultiFormatWriter()
+            val bitMatrix = multiFormatWriter.encode(
+                text,
+                BarcodeFormat.QR_CODE,
+                300,
+                200)
+
+            BarcodeEncoder().createBitmap((bitMatrix))
+        } catch (ex: java.lang.Exception) {
+            null
+        }
+    }
+
+    //binding.emptyImage.setImageBitmap(getBitmapFromString(text: ))
+}
