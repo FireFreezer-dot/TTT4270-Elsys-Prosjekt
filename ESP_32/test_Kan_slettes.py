152847eda62ea1e2e8bdd44278358d677d0a1c27
FireFreezer-dot <eirik.hoel@live.com>
2026-03-23 15:32:08 +0100

    la til et plotte ting for bluetooth avstand

---
+++ ESP_32/test_Kan_slettes.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([2, 3, 5, 7, 11])
+
+print("start")
+
+#plt.figure(figsize=(12, 8))
+
+plt.plot(x,y)
+plt.show()
+
+print("end")
+
